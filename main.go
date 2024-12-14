c735bb23719c7c69fb1a50e806f747701cc5b2df
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-12-14 10:21:33 +0100

    unfeat: remove postgres storage and use built-in inMemoryStorage instead

--- main.go
+++ main.go
@@ -11,11 +11,9 @@ import (
 	"time"
 
 	"github.com/gocolly/colly"
-	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/nlnwa/whatwg-url/canonicalizer"
 	"github.com/nlnwa/whatwg-url/url"
-	"github.com/zolamk/colly-postgres-storage/colly/postgres"
 )
 
 const BATCH_SIZE = 1024
@@ -222,18 +220,6 @@ func GetUrlHash(url url.Url) int64 {
 }
 
 func main() {
-	// Load Settings
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	s := Settings{
-		PostgresUser:     os.Getenv("POSTGRES_USER"),
-		PostgresPassword: os.Getenv("POSTGRES_PASSWORD"),
-		PostgresHost:     os.Getenv("POSTGRES_HOST"),
-		PostgresOptions:  os.Getenv("POSTGRES_OPTIONS"),
-	}
-
 	// Init Backlink engine DB Connection
 	initSqlite()
 	defer db.Close()
@@ -253,23 +239,6 @@ func main() {
 	)
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 8})
 	c.SetRequestTimeout(5 * time.Second)
-
-	// Set Colly Storage
-	storage_uri := fmt.Sprintf(
-		"postgres://%s:%s@%s/colly?%s",
-		s.PostgresUser,
-		s.PostgresPassword,
-		s.PostgresHost,
-		s.PostgresOptions,
-	)
-	storage := &postgres.Storage{
-		URI:          storage_uri,
-		VisitedTable: "colly_visited",
-		CookiesTable: "colly_cookies",
-	}
-	if err := c.SetStorage(storage); err != nil {
-		log.Fatal(err)
-	}
 
 	// Handlers
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
