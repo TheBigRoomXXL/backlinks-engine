4a39f902f154254f85d8d5a5194231613530dd2d
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-12-31 13:05:17 +0100

    wip: utils and tests

---
+++ internal/utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"slices"
+	"strings"
+)
+
+func ReverseHostname(hostname string) string {
+	labels := strings.Split(hostname, ".")
+	slices.Reverse(labels)
+	return strings.Join(labels, ".")
+}
