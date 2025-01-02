0c6d439a2387a0a5a46950c9e769bdcd773dba26
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-01-02 22:29:41 +0100

    feat: very basic url normalizer

--- internal/commons/commons.go
+++ internal/commons/commons.go
@@ -2,6 +2,8 @@ package commons
 
 import (
 	"context"
+	"fmt"
+	"net/url"
 	"slices"
 	"strings"
 	"time"
@@ -11,6 +13,28 @@ func ReverseHostname(hostname string) string {
 	labels := strings.Split(hostname, ".")
 	slices.Reverse(labels)
 	return strings.Join(labels, ".")
+}
+
+func NormalizeUrl(url *url.URL) (*url.URL, error) {
+	// TODO: bring back the rules from GoogleSafeBrowsing in a performant way
+	// url, err := canonicalizer.GoogleSafeBrowsing.Parse(urlRaw)
+	if url.Scheme == "" {
+		url.Scheme = "http"
+	}
+
+	if url.Scheme != "http" && url.Scheme != "https" {
+		return nil, fmt.Errorf("url scheme is not http or https: %s", url.Scheme)
+	}
+
+	p := url.Port()
+	if p != "" && p != "80" && p != "443" {
+		return nil, fmt.Errorf("port is not 80 or 443: %s", p)
+	}
+	url.Host = url.Hostname()
+	url.Fragment = ""
+	url.RawQuery = ""
+
+	return url, nil
 }
 
 // Delay returns nil after the specified duration or error if interrupted.
