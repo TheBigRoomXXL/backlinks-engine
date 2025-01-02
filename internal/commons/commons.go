c2ae0cc2b0ab8857846290a498ec6e7d57473669
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-01-02 09:50:02 +0100

    feat: add context cancelation and timeouts to http client

--- internal/commons/commons.go
+++ internal/commons/commons.go
@@ -1,12 +1,26 @@
 package commons
 
 import (
+	"context"
 	"slices"
 	"strings"
+	"time"
 )
 
 func ReverseHostname(hostname string) string {
 	labels := strings.Split(hostname, ".")
 	slices.Reverse(labels)
 	return strings.Join(labels, ".")
+}
+
+// Delay returns nil after the specified duration or error if interrupted.
+func Delay(ctx context.Context, d time.Duration) error {
+	t := time.NewTimer(d)
+	select {
+	case <-ctx.Done():
+		t.Stop()
+		return ctx.Err()
+	case <-t.C:
+		return nil
+	}
 }
