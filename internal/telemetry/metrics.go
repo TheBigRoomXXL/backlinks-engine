c8a6dbcab721b418045fd2c9ee4daf44bc8e790c
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-01-04 09:02:13 +0100

    misc: add queue size to metrics

--- internal/telemetry/metrics.go
+++ internal/telemetry/metrics.go
@@ -11,18 +11,20 @@ import (
 var (
 	ProcessedURL *expvar.Int
 	Errors       *expvar.Int
+	QueueSize    *expvar.Int
 )
 
 func init() {
 	ProcessedURL = expvar.NewInt("PocessedURL")
 	Errors = expvar.NewInt("Errors")
+	QueueSize = expvar.NewInt("QueueSize")
 }
 
 func MetricsReport(ctx context.Context) {
 	start := time.Now()
-	fmt.Println("┌───────────────┬───────────────┬───────────────┐")
-	fmt.Println("│     Time      │   processed   │    errors     │")
-	fmt.Println("├───────────────┼───────────────┼───────────────┤")
+	fmt.Println("┌───────────────┬───────────────┬───────────────┬───────────────┐")
+	fmt.Println("│     Time      │   processed   │    errors     │  queue size   │")
+	fmt.Println("├───────────────┼───────────────┼───────────────┼───────────────┤")
 
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
@@ -30,13 +32,13 @@ func MetricsReport(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("\r└───────────────┴───────────────┴───────────────┘")
+			fmt.Println("\r└───────────────┴───────────────┴───────────────┴───────────────┘")
 			return
 		case <-ticker.C:
 			time := time.Since(start).Round(time.Second)
 			fmt.Printf(
-				"│ %13s │ %13d │ %13d │\n",
-				time, ProcessedURL.Value(), Errors.Value(),
+				"│ %13s │ %13d │ %13d │ %13d │\n",
+				time, ProcessedURL.Value(), Errors.Value(), QueueSize.Value(),
 			)
 		}
 	}
