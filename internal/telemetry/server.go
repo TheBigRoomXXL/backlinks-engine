93b42af6145b53254eaa1f5e91ecae636d7ccf1e
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-01-15 16:59:30 +0100

    misc: basic prometheus setup

--- internal/telemetry/server.go
+++ internal/telemetry/server.go
@@ -6,9 +6,15 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func StartTelemetryServer(listen string) {
-	fmt.Printf("Telemetry server is listening on http://%s/debug/pprof and http://%s/debug/vars \n", listen, listen)
-	log.Fatalf("pprof crashed: %s", http.ListenAndServe(listen, nil))
+	http.Handle("/metrics", promhttp.Handler())
+	fmt.Println("Telemetry server is listening on:")
+	fmt.Printf("  - http://%s/debug/pprof  \n", listen)
+	fmt.Printf("  - http://%s/debug/vars \n", listen)
+	fmt.Printf("  - http://%s/metrics \n", listen)
+	log.Fatalf("Telemetry server crashed: %s", http.ListenAndServe(listen, nil))
 }
